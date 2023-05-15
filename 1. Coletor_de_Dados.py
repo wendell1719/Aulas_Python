3fa51cc83f4676190fd6d124eeabca62f06ea248
alex <ribeiro.alex.avw@gmail.com>
2023-05-15 13:18:46 -0300

    alteração do projeto

---
+++ 1. Coletor_de_Dados.py
@@ -0,0 +1,11 @@
+#nome = input ("qual é seu nome ?")
+#print (f"seu nome é {nome}")
+
+Numero_1 = int (input(f" digite um numero "))
+Numero_2 = int (input(f" digite outro numero "))
+
+int_Numero_1 = int (Numero_1)
+int_numero_2 = int (Numero_2)
+
+
+soma = print(f"a soma é : {Numero_1 + Numero_2}") 
