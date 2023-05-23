abc0afc7a0985501c0ca7c8181b3ac18e7b196ae
alex <ribeiro.alex.avw@gmail.com>
2023-05-23 15:37:07 -0300

    Aulas Python

---
+++ Aula 2 If elif else.py
@@ -0,0 +1,15 @@
+
+condição1 = False
+condição2 = True
+condição3 = False
+
+if condição1 :
+    print ("Ira execultar a condição 1")
+elif condição2 :
+    print ("Ira execultar a condição 2 ")
+elif condição3 :
+     print ("Ira execultar a condição 3")
+else:
+    print("Ira execultar nada")
+ 
+ 
