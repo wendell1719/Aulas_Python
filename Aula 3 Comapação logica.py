b18d3a6cdbdbe90461a4b70fbb79edaaac549ea1
alex <ribeiro.alex.avw@gmail.com>
2023-05-29 18:13:52 -0300

    Atuzalização

---
+++ Aula 3 Comapação logica.py
@@ -0,0 +1,13 @@
+primeiro_valor = input ("Digite o primeiro Numero :")
+segundo_valor = input ("Digite o segundo Numero : ")
+
+
+if primeiro_valor >= segundo_valor:
+    print(f"{primeiro_valor=} é maior"   
+     f"ou igual do que{segundo_valor=}") 
+
+else: 
+    print (f"{segundo_valor=} é maior "
+     f"o igual do que {primeiro_valor=}")
+
+
