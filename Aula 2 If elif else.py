1ca7f74448103420e9c17666cc35f17cddb0d2a8
alex <ribeiro.alex.avw@gmail.com>
2023-05-29 14:20:27 -0300

    Coposição logica

--- Aula 2 If elif else.py
+++ Aula 2 If elif else.py
@@ -1,14 +1,18 @@
+# condiçoes para ser execultas
+condição1 = False # não vai ser lida no If
+condição2 = True # vai ser lida no if pela condiação ser verdadeira 
+condição3 = False #Não vai ser lida no if
 
-condição1 = False
-condição2 = True
-condição3 = False
-
+#primeira condição 
 if condição1 :
     print ("Ira execultar a condição 1")
+#segunda condição
 elif condição2 :
     print ("Ira execultar a condição 2 ")
+#terceira condição
 elif condição3 :
      print ("Ira execultar a condição 3")
+#quarta e ultima condição finalizando com else 
 else:
     print("Ira execultar nada")
  
