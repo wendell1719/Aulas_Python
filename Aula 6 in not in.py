c8ce0dbea6b773c4a1cc31690ef5aa1923dd196c
alex <ribeiro.alex.avw@gmail.com>
2023-09-12 14:32:58 -0300

    atualização

---
+++ Aula 6 in not in.py
@@ -0,0 +1,9 @@
+nome = input ("Digite seu nome")
+encontrar = input ("oque deseja encontrar ")
+
+if encontrar in nome :
+    print(f"{encontrar}esta em {nome}")
+
+else: 
+    print(f"{encontra}Não esta em {nome}") 
+
